56f828fd30e5c1202022eb9c2f1daba0491afd58
MilderBronze <sumitinbki@gmail.com>
2026-01-04 10:59:57 +0530

    added auth, middlemenu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import Link from "next/link"
 import React from "react"
 
@@ -7,7 +8,7 @@ export default function MobileMenu() {
     const [isOpen, setIsOpen] = React.useState(false)
 
     return (
-        <div>
+        <div className="md:hidden">
             <div className="flex flex-col gap-[4.5px] cursor-pointer" onClick={() => setIsOpen(prev => !prev)}>
                 <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "rotate-45" : ""} origin-left ease-in-out duration-500`} />
                 <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "opacity-0" : ""}  ease-in-out duration-500`} />
